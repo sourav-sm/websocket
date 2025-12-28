9e270639b8449ba5f830f90e75da9ce918f6f340
SOURAV <souravmohanta4428@gmail.com>
2025-12-29 01:32:07 +0530

    final commit

---
+++ backend/dist/index.js
@@ -0,0 +1,16 @@
+import { WebSocketServer } from 'ws';
+const wss = new WebSocketServer({ port: 8080 });
+//event handler --->like app.get("/users",{}); on express https
+wss.on("connection", function (socket) {
+    console.log("user connectted");
+    // setInterval(() => {
+    //     socket.send("Curr price of zomato share is " + Math.random());
+    // }, 500);
+    //---NOW LETS BUILD A PING PONG APPICATION WHERE IF WE SEND PING IT WILL RETURN PONG
+    socket.on("message", (e) => {
+        if (e.toString() === "ping") {
+            socket.send("pong");
+        }
+    });
+});
+//# sourceMappingURL=index.js.map
